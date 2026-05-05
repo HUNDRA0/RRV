fbb6ae4e30a93a7f1fe2c0ae7f03286f8f2e9636
Jacob Ercan <jacobercan@AirsomthorJacob.lan>
2026-05-05 16:14:09 +0200

    Set Jobblistan initial order via migration

---
+++ server/migrations/010_lb_order.sql
@@ -0,0 +1,4 @@
+INSERT OR REPLACE INTO site_content (key, value) VALUES (
+  'lb_order',
+  '["fredrik","mario","adam","gab","emanuel","christian","jovo","ninos","andre","jacob","john","george","joseph","robin","ninmar","joel"]'
+);
